724ad5a38bb60efd384c4492baf148908b24b86b
Incept1on <incept1onnn999@gmail.com>
2024-11-24 18:57:50 +0300

    init commit

---
+++ server1/server1.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get("/", response_class=HTMLResponse)
+async def main():
+    with open("./server1.html", "r") as html_file:
+        html_return = html_file.read()
+    return html_return
